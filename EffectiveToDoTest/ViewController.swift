45f3d21bd1815cd02617f81e0e2a8c6236894b14
Ivan Korobov <ikorobov.dev@gmail.com>
2025-04-04 15:12:19 +0200

    Initial Commit

---
+++ EffectiveToDoTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EffectiveToDoTest
+//
+//  Created by ikorobov on 4. 4. 2025..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
